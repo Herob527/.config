18757f27d1132f72bddbea745e950ca0584d62ea
Herob527 <szymon.wrzos@onet.pl>
2025-07-11 16:17:05 +0200

    Add currently used stuff

---
+++ fish/conf.d/npm_aliases.fish
@@ -0,0 +1,166 @@
+#!/usr/bin/env fish
+
+# Function to detect the appropriate package manager based on lock files
+function detect_package_manager
+    if test -f "bun.lockb"; or test -f "bun.lock"
+        echo "bun"
+    else if test -f "pnpm-lock.yaml"
+        echo "pnpm"
+    else if test -f "yarn.lock"
+        echo "yarn"
+    else if test -f "package-lock.json"; or test -f "package.json"
+        echo "npm"
+    else
+        echo "npm" # Default to npm if no lock file is found
+    end
+end
+
+# Function to recommend what package manager to use
+function which_pm
+    set -l pm (detect_package_manager)
+    
+    echo "Detected package manager: $pm"
+    
+    switch $pm
+        case "bun"
+            echo "Commands to use:"
+            echo "  - Install dependencies: bun install"
+            echo "  - Add a package: bun add [package]"
+            echo "  - Run a script: bun run [script]"
+        case "pnpm"
+            echo "Commands to use:"
+            echo "  - Install dependencies: pnpm install"
+            echo "  - Add a package: pnpm add [package]"
+            echo "  - Run a script: pnpm [script]"
+        case "yarn"
+            echo "Commands to use:"
+            echo "  - Install dependencies: yarn"
+            echo "  - Add a package: yarn add [package]"
+            echo "  - Run a script: yarn [script]"
+        case "npm"
+            echo "Commands to use:"
+            echo "  - Install dependencies: npm install"
+            echo "  - Add a package: npm install [package]"
+            echo "  - Run a script: npm run [script]"
+    end
+end
+
+# Function to handle both install and add commands
+function ni
+    set -l pm (detect_package_manager)
+    
+    # If no arguments, run the install command
+    if test (count $argv) -eq 0
+        switch $pm
+            case "bun"
+                bun install
+            case "pnpm"
+                pnpm install
+            case "yarn"
+                yarn
+            case "npm"
+                npm install
+        end
+    # Otherwise, run the add command with arguments
+    else
+        switch $pm
+            case "bun"
+                bun add $argv
+            case "pnpm"
+                pnpm add $argv
+            case "yarn"
+                yarn add $argv
+            case "npm"
+                npm install $argv
+        end
+    end
+end
+
+# Function to run the appropriate run command
+function nr
+    set -l pm (detect_package_manager)
+    
+    switch $pm
+        case "bun"
+            bun run $argv
+        case "pnpm"
+            pnpm $argv[1] $argv[2..-1]
+        case "yarn"
+            yarn $argv[1] $argv[2..-1]
+        case "npm"
+            npm run $argv
+    end
+end
+
+function no
+    set -l pm (detect_package_manager)
+    
+    switch $pm
+        case "bun"
+            bun outdated  $argv
+        case "pnpm"
+            pnpm outdated $argv
+        case "yarn"
+            yarn outdated $argv
+        case "npm"
+            npm outdated $argv
+    end
+end
+
+
+function nu
+    set -l pm (detect_package_manager)
+    
+    switch $pm
+        case "bun"
+            bun update $argv
+        case "pnpm"
+            pnpm update $argv
+        case "yarn"
+            yarn upgrade $argv
+        case "npm"
+            npm update $argv
+    end
+end
+
+# Function to start the project (start script or fallback to dev)
+function nst
+    set -l pm (detect_package_manager)
+    
+    # Check if package.json exists and has a start script
+    if test -f "package.json"
+        set -l has_start (jq -r '.scripts.start // empty' package.json 2>/dev/null)
+        if test -n "$has_start"
+            # Start script exists, use it
+            switch $pm
+                case "bun"
+                    bun run start
+                case "pnpm"
+                    pnpm start
+                case "yarn"
+                    yarn start
+                case "npm"
+                    npm start
+            end
+        else
+            # No start script, fallback to dev
+            echo "No 'start' script found, running 'dev' instead..."
+            switch $pm
+                case "bun"
+                    bun run dev
+                case "pnpm"
+                    pnpm dev
+                case "yarn"
+                    yarn dev
+                case "npm"
+                    npm run dev
+            end
+        end
+    else
+        echo "No package.json found"
+        return 1
+    end
+end
+# Register the function as commands
+alias which-pm="which_pm"
+
---
+++ fish/config.fish
@@ -0,0 +1,70 @@
+mise activate fish | source
+
+if status is-interactive
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
+
+
+set EDITOR $(which nvim)
+set VISUAL $(which nvim)
+starship init fish | source
+fish_add_path -g 
+
+set -gx PATH "/run/user/1000/fnm_multishells/22028_1736889187602/bin" $PATH;
+set PATH "$PATH:$HOME/.local/share/bob/nvim-bin"
+set PATH "$PATH:/home/szymon/.dotnet/tools"
+set -gx FNM_MULTISHELL_PATH "/run/user/1000/fnm_multishells/22028_1736889187602";
+set -gx FNM_VERSION_FILE_STRATEGY "local";
+set -gx FNM_DIR "$HOME/.local/share/fnm";
+set -gx FNM_LOGLEVEL "info";
+set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist";
+set -gx FNM_COREPACK_ENABLED "false";
+set -gx FNM_RESOLVE_ENGINES "true";
+set -gx FNM_ARCH "x64";
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH $HOME/.ghcup/bin # ghcup-env
+
+
+type -q node
+
+if test $status -eq 1
+  fnm use default
+end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+function git_profile -d "Set git user name and email" -a profile
+  set value $(echo $profile | string lower)
+  if test $value = "work"
+    git config user.name "Szymon Wrzos"
+    git config user.email "szymon.wrzos@bitapps.fi"
+  else if test $value = "herob"
+    git config user.name "Herob527"
+    git config user.email "szymon.wrzos@onet.pl"
+  else 
+    echo "usage: git_profile <work|herob>"
+  end
+
+end
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+alias atup "sudo dnf upgrade && flatpak upgrade"
+alias lg "lazygit"
+alias doup "docker compose up"
+alias doupw "docker compose up --watch"
+alias dodo "docker compose down"
+# Used to run neovim with poetry to apply poetry-managed virtual env
+alias pvim "poetry run nvim"
+alias svim "EDITOR=$(which nvim) sudoedit"
+alias gmtl "git mergetool"
+alias ls "eza --header --git --icons -w 64"
+
