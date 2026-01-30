2317b762e2a175399e71ffbb5316a7443193cbb2
szymon <szymon@debian>
2026-01-30 01:36:39 +0100

    Remove loading

--- fish/conf.d/virtualfish-loader.fish
+++
@@ -1,5 +0,0 @@
-set -g VIRTUALFISH_VERSION 2.5.9
-set -g VIRTUALFISH_PYTHON_EXEC /home/szymon/.local/share/mise/installs/pipx-virtualfish/2.5.9/venvs/virtualfish/bin/python
-source /home/szymon/.local/share/mise/installs/pipx-virtualfish/2.5.9/venvs/virtualfish/lib64/python3.13/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
-
