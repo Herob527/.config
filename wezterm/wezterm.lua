388a70370f812b3f830cb927f7a3b88f0790ad53
Herob527 <szymon.wrzos@onet.pl>
2026-02-01 14:38:52 +0100

    Remove comments

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,23 +15,15 @@ config.window_padding = {
 }
 
 config.keys = {
-	-- CTRL-SHIFT-l activates the debug overlay
 	{ key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
 }
 local frappe = wezterm.color.get_builtin_schemes()["catppuccin-frappe"]
 
--- This function returns the suggested title for a tab.
--- It prefers the title that was set via `tab:set_title()`
--- or `wezterm cli set-tab-title`, but falls back to the
--- title of the active pane in that tab.
 function tab_title(tab_info)
 	local title = tab_info.tab_title
-	-- if the tab title is explicitly set, take that
 	if title and #title > 0 then
 		return title
 	end
-	-- Otherwise, use the title from the active pane
-	-- in that tab
 	return tab_info.active_pane.title
 end
 
@@ -39,48 +31,33 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 	local title = tab_title(tab)
 	if tab.is_active then
 		return {
+
 			{ Background = { Color = frappe.selection_bg } },
 			{ Text = title },
 		}
 	end
 	if tab.is_last_active then
-		-- Green color and append '*' to previously active tab.
 		return {
 			{ Background = { Color = frappe.background } },
+
 			{ Text = title },
 		}
 	end
 	return title
 end)
+
 config.font = wezterm.font("InconsolataNerdFont")
 config.font_size = 14
 config.color_scheme = "Catppuccin Frappe"
 
 config.window_frame = {
-	-- The font used in the tab bar.
-	-- Roboto Bold is the default; this font is bundled
-	-- with wezterm.
-	-- Whatever font is selected here, it will have the
-	-- main font setting appended to it to pick up any
-	-- fallback fonts you may have used there.
 	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-
-	-- The size of the font in the tab bar.
-	-- Default to 10.0 on Windows but 12.0 on other systems
 	font_size = 12.0,
-
-	-- The overall background color of the tab bar when
-	-- the window is focused
 	active_titlebar_bg = "#292c3c",
-
-	-- The overall background color of the tab bar when
-	-- the window is not focused
 	inactive_titlebar_bg = "#303446",
 }
 
 config.keys = {
-	-- Turn off the default CMD-m Hide action, allowing CMD-m to
-	-- be potentially recognized and handled by the tab
 	{
 		key = "/",
 		mods = "CTRL|SHIFT",
