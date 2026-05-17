9adb4367a93629eba9ed17c4ee18548ffc58eba4
Herob527 <szymon.wrzos@onet.pl>
2026-05-17 13:28:05 +0200

    Improvement

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,23 +1,32 @@
 local wezterm = require("wezterm")
-
 local act = wezterm.action
 local config = {}
 
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+-- 1. Basic Structural Settings (Lines 10-15)
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+-- 2. Font and Appearance
+config.font = wezterm.font({ family = "JetBrains Mono", weight = "Light" })
+config.font_size = 12
+config.color_scheme = "Catppuccin Frappe"
+config.window_frame = {
+	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
+	font_size = 12.0,
+	active_titlebar_bg = "#292c3c",
+	inactive_titlebar_bg = "#303446",
 }
 
+-- 3. Custom Key Bindings (Consolidated)
 config.keys = {
+    -- Debug Overlay Shortcut
 	{ key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
+    -- Split Panes Shortcuts
+	{ key = "/", mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal },
+	{ key = "+", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical },
+	{ key = "-", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical },
 }
 
+-- 4. Tab Title Formatting Hook (Lines 21-48)
 local frappe = wezterm.color.get_builtin_schemes()["catppuccin-frappe"]
 
 function tab_title(tab_info)
@@ -32,7 +41,6 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 	local title = tab_title(tab)
 	if tab.is_active then
 		return {
-
 			{ Background = { Color = frappe.selection_bg } },
 			{ Text = title },
 		}
@@ -40,39 +48,11 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 	if tab.is_last_active then
 		return {
 			{ Background = { Color = frappe.background } },
-
 			{ Text = title },
 		}
 	end
 	return title
 end)
 
-config.font = wezterm.font({ family = "JetBrains Mono", weight = "Light" })
-config.font_size = 12
-config.color_scheme = "Catppuccin Frappe"
-
-config.window_frame = {
-	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-	font_size = 12.0,
-	active_titlebar_bg = "#292c3c",
-	inactive_titlebar_bg = "#303446",
-}
-
-config.keys = {
-	{
-		key = "/",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitHorizontal,
-	},
-	{
-		key = "+",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitVertical,
-	},
-	{
-		key = "-",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitVertical,
-	},
-}
+-- 5. Return Config (Line 78)
 return config
